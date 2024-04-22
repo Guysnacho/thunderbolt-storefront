2ff6aa6664afc6cf4d03945dbbacdd74eb6f3967
Samuel Adetunji <guysnacho2@gmail.com>
2024-04-22 04:00:47 -0500

    Trim footer till we have official pages

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -103,6 +103,8 @@ export default async function Footer() {
                 </ul>
               </div>
             )}
+            {/* 
+            Truncate footer a bit
             <div className="flex flex-col gap-y-2">
               <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
               <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
@@ -137,7 +139,7 @@ export default async function Footer() {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -22,7 +22,7 @@ export default async function Nav() {
             <div className="flex items-center h-full">
               <LocalizedClientLink
                 href="/"
-                className="txt-compact-xlarge-plus text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                className="txt-compact-xlarge-plus uppercase text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                 data-testid="nav-store-link"
               >
                 QAutos Preowned
